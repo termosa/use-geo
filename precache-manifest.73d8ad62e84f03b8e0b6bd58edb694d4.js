1d5d638fbdabe1de0290a781fe9297cce3f115b6
Stanislav Termosa <termosa.stanislav@gmail.com>
2020-11-16 00:06:06 +0200

    Updates

---
+++ precache-manifest.73d8ad62e84f03b8e0b6bd58edb694d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaafe0bc3f29539a4c7e5e8a5e11e14",
+    "url": "/use-geo/index.html"
+  },
+  {
+    "revision": "830a20130f882af88bb7",
+    "url": "/use-geo/static/css/main.12fd5197.chunk.css"
+  },
+  {
+    "revision": "f35e0e708a107671ded6",
+    "url": "/use-geo/static/js/2.5465470b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-geo/static/js/2.5465470b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830a20130f882af88bb7",
+    "url": "/use-geo/static/js/main.f9a0a65e.chunk.js"
+  },
+  {
+    "revision": "0946b7645d43f483001c",
+    "url": "/use-geo/static/js/runtime-main.fba8d32a.js"
+  }
+]);
